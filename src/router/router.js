87104f35638b3d7f0dd4465280c1fea48ca0847f
xiongjing <1161033437@qq.com>
2021-11-18 23:56:22 +0800

    home

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,7 @@
 const routes = [
     { path: '/', component: resolve => require(['../pages/home'], resolve)},
     { path: '/home', component: resolve => require(['../pages/home'], resolve)},
+    { path: '/sy', component: resolve => require(['../pages/sy'], resolve)},
     { path: '/farm', component: resolve => require(['../pages/farm'], resolve)},
 
 ];
